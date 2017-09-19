17876733515dae89acf8567d9d4f269f6df9d668
Nicholas Glenn <nick@helpfulhuman.com>
2017-09-18 23:26:34 -0700

    Removed the highlightjs scripts for straight HTML elements

--- info.md
+++
@@ -1,5 +0,0 @@
-## What is this?
-
-**Code A Better World** is an open group for developers who want to use their skills to help others at no cost.  Our goal is to assemble a team of volunteers who can help us build software, websites and more for charities and non-profits.
-
-_More info coming soon._
--- public/index.php
+++ public/index.php
@@ -8,7 +8,6 @@
     <link href="/favicon.ico" rel="shortcut icon" />
     <link href="//fonts.googleapis.com/css?family=Source+Code+Pro|Source+Sans+Pro" rel="stylesheet" />
     <link href="/site.css" rel="stylesheet" />
-    <link href="/highlight.css" rel="stylesheet" />
   </head>
   <body>
     <main>
@@ -17,13 +16,20 @@
         <header class="Window-Bar">
           <h1>codeabetter.world</h1>
         </header>
-        <pre class="Window-Body"><code class="markdown"><?php require("../info.md"); ?></code></pre>
         <section class="Window-Body">
-          Find us on <a href="https://github.com/codeabetterworld">Github</a>
+          <h2>What is this?</h2>
+          <p><strong>**Code A Better World**</strong> is an open group for developers who want to use their skills to help others at no cost.  Our goal is to assemble a team of volunteers who can help us build software, websites and more for charities and non-profits.</p>
+          <p><em>_More info coming soon._</em></p>
+          <p>You can find us on <a href="https://github.com/codeabetterworld">Github</a>.</p>
+          <hr />
+          <div class="Sponsors">
+            <p class="Sponsors-Intro">Code A Better World is sponsored by...</p>
+            <ul>
+              <li><a href="http://helpfulhuman.com">Helpful Human</a></li>
+            </ul>
+          </div>
         </section>
       </article>
     </main>
-    <script src="/highlight.js"></script>
-    <script>hljs.initHighlightingOnLoad();</script>
   </body>
 </html>
