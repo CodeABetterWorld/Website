2ce6e9c5d4675ec8b62e756f030e423bd682b334
Nicholas Glenn <nick@helpfulhuman.com>
2017-09-18 21:33:15 -0700

    Initial commit

---
+++ info.md
@@ -0,0 +1,7 @@
+## What?
+
+## Why?
+That's a question that shouldn't need to be asked.
+
+## How?
+We're still figuring out the details on the how.
---
+++ public/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>codeforabetter.world</title>
+    <meta name="description" content="Code A Better World is an open-group for people who want to help make the world a better place through code." />
+    <link href="//fonts.googleapis.com/css?family=Source+Code+Pro|Source+Sans+Pro" rel="stylesheet" />
+    <link href="/site.css" rel="stylesheet" />
+    <link href="/highlight.css" rel="stylesheet" />
+  </head>
+  <body>
+    <main>
+      <img src="/logo.svg" alt="Code A Better World" class="Logo" />
+      <article class="Window">
+        <header class="Window-Bar">
+          <h1>codeabetter.world</h1>
+        </header>
+        <pre class="Window-Body"><code class="markdown"><?php require("./info.md"); ?></code></pre>
+      </article>
+    </main>
+    <script src="/highlight.pack.js"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
+  </body>
+</html>
